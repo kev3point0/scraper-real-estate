fad5ad77d851ecc37a1b377f3cb659c7de4501e9
Kevin Roy <kevin.roy@owasp.org>
2022-11-03 16:52:53 -0300

    added PropertyGuys POC

--- src/index.js
+++ src/index.js
@@ -5,10 +5,62 @@ const baseURL = "https://www.kijiji.ca";
 const categoryURL = `${baseURL}/b-for-sale/new-brunswick/c30353001l9005`;
 
 (async function main() {
-    await startScraper();
+    //await startKijijiScraper();
+    await startPropertyGuysScraper();
+    //TODO mls.ca
+    //await startMLSScraper();
 })();
 
-async function startScraper() {
+async function startMLSScraper(){
+
+}
+
+async function startPropertyGuysScraper() {
+    let pageId = 1;
+
+    //http request here
+    const listings =  await queryPropertyGuysAPI(pageId);
+
+    //interesting metrics here
+    //check this every 24 hours to see listings amount during recession
+    const totalListingsInNB = listings.listingSearchResult.total;
+
+    const listingList = listings.listingSearchResult.listingList;
+    console.log(listingList);
+
+    //I think I should just send all json to elastic search without parsing it!?!???!!
+    for (const listing in listingList){
+
+        //send to elastic search
+        console.log(listing);
+        console.log(`--------------------------------------------------------------------------`);
+    }
+};
+
+async function queryPropertyGuysAPI(pageId) {
+    const result = await fetch("https://propertyguys.com/api/v1/listings/search", {
+        "credentials": "include",
+        "headers": {
+            "User-Agent": "Mozilla/5.0 (X11; Linux x86_64; rv:106.0) Gecko/20100101 Firefox/106.0",
+            "Accept": "*/*",
+            "Accept-Language": "en-US,en;q=0.5",
+            "Content-Type": "application/x-www-form-urlencoded; charset=UTF-8",
+            "x-user-access-token": "504852:zmHAzSixx6eu8VQmTqXR",
+            "X-Requested-With": "XMLHttpRequest",
+            "Sec-Fetch-Dest": "empty",
+            "Sec-Fetch-Mode": "cors",
+            "Sec-Fetch-Site": "same-origin"
+        },
+        "referrer": "https://propertyguys.com/search/ca/nb/",
+        "body": "search_filter%5BcountryId%5D=1&search_filter%5BprovinceId%5D=4&page=" + pageId + "&size=11&api_key=37a742b97452d422264cd602f4a442d9023786611fa5",
+        "method": "POST",
+        "mode": "cors"
+    });
+
+    return JSON.parse(await result.text());
+}
+
+async function startKijijiScraper() {
     try {
 
         //do http request
@@ -24,12 +76,15 @@ async function startScraper() {
         //loop through all regular ads
         for (const ad of ads) {
 
+            //just send entire ad to elastic search instead of below parsers
+            
             //get info from ads page for each ad
             const ad_url = getAdURL($, ad);
             const title = getAdTitle($, ad);
             const price = getAdPrice($, ad);
             const location = getAdLocation($, ad);
             const description = getAdDescription($, ad);
+            console.log(location);
             const rental_info = getAdRentalInformation($, ad);
             const ad_id = getAdID(ad_url);
             const userId = await getUserID(ad_url);
